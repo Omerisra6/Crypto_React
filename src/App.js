580ea2e6635d24b76796494f64dbf7853febf882
Omerisra06 <Omerisraeli6@gmail.com>
2023-04-11 18:29:19 +0300

    Tweak: Header moved to app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from './components/Header';
 import CryptoCurrency from './pages/CryptoCurrency';
 
 function App() 
 { 
   return (
     <div className='app'>
+      <Header/>
       <CryptoCurrency/>
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,18 @@ export default function Header() {
         <div className="header">
 
             <img className="logo" src="/assets/logojpg.jpg"/>
-
-            <div className="header-button">Buy Crypto</div>
-            <div className="header-button">Finance</div>
-            <div className="header-button">Listing</div>
-            <div className="header-button">Academy</div>
-            <div className="header-button">About Us</div>
+            <div className="header-links-container">
+                <div className="header-button">Buy Crypto</div>
+                <div className="header-button">Finance</div>
+                <div className="header-button">Listing</div>
+                <div className="header-button">Academy</div>
+                <div className="header-button">About Us</div>
+            </div>
+            
 
             <div className="top-right"> 
                 <div className="header-button">English</div>
-                <div className="header-button">Usd</div>
+                <div className="header-button">USD</div>
                 <div className="header-button">Sign Up</div>
                 <div className="log-in">LOG IN</div>
             </div>
